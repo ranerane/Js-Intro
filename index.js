f5302df651bcf74b27d4feed77b58ebddbbe49f4
Rane Ebinne <raneebinne@gmail.com>
2022-06-23 20:27:35 +0100

    display data

---
+++ index.js
@@ -0,0 +1,9 @@
+let x    = prompt("Name?");
+let y  = prompt("Height?");
+let z = prompt("Country?");
+document.getElementById("name").innerHTML= x;
+document.getElementById("height").innerHTML=y;
+document.getElementById("country").innerHTML=z;
+document.getElementById("my-container").style.display="block";
+
+
